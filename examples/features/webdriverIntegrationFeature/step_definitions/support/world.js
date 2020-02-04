22001207aea5be249c68a60e55c51d48c9faf05a
DiscoElevator <DiscoElevator@users.noreply.github.com>
2020-02-04 16:03:58 +0300

    Make selenium-webdriver example run chrome in headless mode

--- examples/features/webdriverIntegrationFeature/step_definitions/support/world.js
+++ examples/features/webdriverIntegrationFeature/step_definitions/support/world.js
@@ -1,13 +1,17 @@
 require('chromedriver');
 const cucumber = require('cucumber');
 const { Builder } = require('selenium-webdriver');
+const { Options } = require('selenium-webdriver/chrome');
 const { Logger } = require('../../../../../modules');
 
 class CustomLogger extends Logger {
   constructor(...args) {
     super(...args);
 
-    global.browser = new Builder().forBrowser('chrome').build();
+    global.browser = new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(new Options().headless())
+      .build();
   }
 }
 
